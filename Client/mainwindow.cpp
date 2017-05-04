345ac52eb5fcd6aefee6e26ca55a14793f2d4db5
Théo Poizat <theo.poizat@etu.unistra.fr>
2017-05-04 22:35:55 +0200

    AJout des noms

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -1,3 +1,7 @@
+/* AUTHOR : SUTTER Nicolas et POIZAT Théo
+ * L3 CMI ISR
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "serveur.h"
--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -1,3 +1,7 @@
+/* AUTHOR : SUTTER Nicolas et POIZAT Théo
+ * L3 CMI ISR
+ */
+
 #include "serveurcentral.h"
 #include <QCoreApplication>
 
